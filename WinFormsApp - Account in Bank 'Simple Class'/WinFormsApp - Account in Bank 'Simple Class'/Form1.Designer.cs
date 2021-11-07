9ee27d53bd908524527f36ade6243a9fc5dfceed
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2021-11-07 04:22:48 +0200

    First Edition

---
+++ WinFormsApp - Account in Bank 'Simple Class'/WinFormsApp - Account in Bank 'Simple Class'/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace WinFormsApp___Account_in_Bank__Simple_Class_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Raleway ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(4, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(275, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(488, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Your Balance";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Variable Text", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(12, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(1054, 47);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Raleway ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(362, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose action you want";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Deposit",
+            "Withdrow"});
+            this.comboBox1.Location = new System.Drawing.Point(389, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 37);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Raleway ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 42);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter The Amount";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(309, 283);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(104, 39);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.Font = new System.Drawing.Font("Zilla Slab", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(359, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 51);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Brown;
+            this.button3.Font = new System.Drawing.Font("Zilla Slab", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(93, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 51);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(40, 435);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(726, 43);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1492, 783);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Account at Bank";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WinFormsApp - Account in Bank 'Simple Class'/WinFormsApp - Account in Bank 'Simple Class'/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp___Account_in_Bank__Simple_Class_
+{
+
+    public partial class Form1 : Form
+    {
+        static string NameIN;
+        static double balance1=10000;
+        static double balance2=9481;
+        static double amount;
+        static string textBox;
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            amount = double.Parse(textBox3.Text);
+        }
+
+        class Account
+        {
+            private double money_in_an_Account;
+            private string name;
+            public Account()
+            {
+                this.name = "";
+                this.money_in_an_Account = 0;
+            }
+            public Account(string n, double m)
+            {
+                this.name = n;
+                this.money_in_an_Account = m;
+            }
+            public string NAME
+            {
+                set
+                {
+                    this.name = value;
+                }
+                get
+                {
+                    return this.name;
+                }
+            }
+            public double Money_In_an_Account
+            {
+                set
+                {
+                    this.money_in_an_Account = value;
+                }
+                get
+                {
+                    return this.money_in_an_Account;
+                }
+            }
+           public void Deposit()
+            {
+                if ((amount >= 50) && (NameIN == "Mahmoud" || NameIN == "mahmoud"))
+                {
+                    balance1 += amount;
+                    textBox = "The transaction was completed successfully and you have now " + balance1.ToString() + "$";
+                }
+                
+                else if ((amount >= 50) && (NameIN == "Ahmed" || NameIN == "ahmed"))
+                {
+                    balance2 += amount;
+                    textBox = "The transaction was completed successfully and you have now " + balance2.ToString() + "$";
+                }
+                else textBox = "The minimum deposit amount is 50 $";
+            }
+           public void Withdrow()
+            {
+                if ((amount >= 50) && (NameIN == "Mahmoud" || NameIN == "mahmoud"))
+                {
+                    balance1 -= amount;
+                    textBox = "The transaction was completed successfully and you have now " + balance1.ToString() + "$";
+                }
+                
+                else if ((amount >= 50) && (NameIN == "Ahmed" || NameIN == "ahmed"))
+                {
+                    balance2 -= amount;
+                    textBox = "The transaction was completed successfully and you have now " + balance2.ToString() + "$";
+                }
+                else textBox = "The minimum withdrawal amount is 50 $";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            NameIN=textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(NameIN=="Mahmoud"|| NameIN == "mahmoud")
+            {
+                textBox2.Text = "Hello Mr/Mahmoud ^__^ Your account balance is " + balance1.ToString() + "$";
+            }
+            else if (NameIN == "Ahmed" || NameIN == "ahmed")
+            {
+                textBox2.Text = "Hello Mr/Ahmed ^__^ Your account balance is " +balance2.ToString()+"$";
+            }
+            else
+            {
+                textBox2.Text = "wrong name!!!";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Account Client1 = new Account("Mahmoud", balance1);
+            Account Client2 = new Account();
+            Client2.Money_In_an_Account = balance2;
+            Client2.NAME = "Ahmed";
+            if ((comboBox1.Text == "Deposit") && (NameIN == "Mahmoud" || NameIN == "mahmoud"))
+            {
+                Client1.Deposit(); textBox4.Text = textBox;
+            }
+            else if((comboBox1.Text == "Withdrow") && (NameIN == "Mahmoud" || NameIN == "mahmoud"))
+            {
+                Client1.Withdrow(); textBox4.Text = textBox;
+            }
+            if ((comboBox1.Text == "Deposit") && (NameIN == "Ahmed" || NameIN == "ahmed"))
+            {
+                Client2.Deposit(); textBox4.Text = textBox;
+            }
+            else if ((comboBox1.Text == "Withdrow") && (NameIN == "Ahmed" || NameIN == "ahmed"))
+            {
+                Client2.Withdrow(); textBox4.Text = textBox;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (NameIN == "Mahmoud" || NameIN == "mahmoud")
+                textBox4.Text= "The transaction is Canceled you have"+ balance1.ToString() + "$";
+            else if (NameIN == "Ahmed" || NameIN == "ahmed")
+                textBox4.Text = "The transaction is Canceled you have" + balance2.ToString() + "$";
+        }
+    }
+}
